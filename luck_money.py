3ed3171ed5df9991968fe92d7cc73e8e695215fa
meng <59571513@qq.com>
2017-07-27 00:36:18 +0800

    add frame

---
+++ luck_money.py
@@ -0,0 +1,14 @@
+import pygame
+
+class Main:
+	def __init__(self):
+		print("init main");
+		pass;
+
+	def update(self):
+		print("update");
+		pass;
+
+if __name__ == "__main__":
+	game = Main();
+	game.update();
