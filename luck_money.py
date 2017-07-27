534f1bed186ea02f4648cb4087f867e4d0d3e0a9
meng <59571513@qq.com>
2017-07-27 21:27:59 +0800

    add code

--- luck_money.py
+++ luck_money.py
@@ -1,5 +1,5 @@
 import pygame
-
+import random
 class Main:
 	def __init__(self):
 		pygame.init();
@@ -9,10 +9,47 @@ class Main:
 		pygame.display.set_caption("luck_money");
 		self.unit = pygame.image.load("res/unit.png");
 		self.__bar_stride = 8;
+		self.__gamers = [];
+		self.__gamer_count = 100;
+		for i in range(0, self.__gamer_count):
+			self.__gamers.append(100);
 	def __message_process(self):
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				exit();
+
+	def __play_game(self):
+		idx_list = range(100);
+		for idx in idx_list:
+			self.__update_lucky_money(idx, idx_list);
+		
+	def __update_lucky_money(self, give_money_gamer_index, idx_list):
+		lucky_money = 10;
+		idx_list = range(100);
+		random.shuffle(idx_list);
+
+		average = money / gamer_count;
+		r = random.random();
+		min_value = 0.01;
+		max_value = average * 2.0;
+		val = max_value * r + min_value * (1 - r);
+
+		v = self.__gamers[give_money_gamer_index] - val;
+		if v < 0.0:
+			self.__gamers[give_money_gamer_index] = 0.0;
+		else:
+			self.__gamers[give_money_gamer_index] = v;
+
+		for idx in range(0, self.__gamer_count):
+			if idx != give_money_gamer_index:
+				self.__gamers[idx] = self.__gamers[idx] + val;
+
+		rest_money = money - val;
+
+		if rest_money < 0.0:
+			rest_money = 0.0;
+
+		return rest_money;
 
 	def update(self):
 		while True:
